4509b5381d52ba6104c3e4ad299ca85791eb499a
Asmita Tele <asmitatele03@gmail.com>
2026-02-28 22:41:58 +0530

    Update lcd.h

--- arduino-i2c-lcd-display/src/lcd.h
+++ arduino-i2c-lcd-display/src/lcd.h
@@ -1,1 +1,14 @@
+#ifndef LCD_H
+#define LCD_H
 
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include "delay.h"     // include delay header
+
+// I2C LCD object declaration
+extern LiquidCrystal_I2C lcd;
+
+void lcd_init();
+void lcd_print_lines(const char *line1, const char *line2);
+
+#endif
