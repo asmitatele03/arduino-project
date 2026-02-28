658b93961f758f6c803dc8adbb0d393d1dc63143
Asmita Tele <asmitatele03@gmail.com>
2026-02-28 22:43:02 +0530

    Update lcd.cpp

--- arduino-i2c-lcd-display/src/lcd.cpp
+++ arduino-i2c-lcd-display/src/lcd.cpp
@@ -1,1 +1,19 @@
+#include <Arduino.h>
+#include "lcd_.h"
+
+// Create LCD object at I2C address 0x27
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void lcd_init() {
+    lcd.init();
+    lcd.backlight();
+}
+
+void lcd_print_lines(const char *line1, const char *line2) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(line1);
+    lcd.setCursor(0, 1);
+    lcd.print(line2);
+}
 
