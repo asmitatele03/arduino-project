0ac1c6c8c77a2938b3a2b900a755c01c6aa1aa19
Asmita Tele <asmitatele03@gmail.com>
2026-02-28 22:41:00 +0530

    Update lcd_i2c_project.ino

--- arduino-i2c-lcd-display/src/lcd_i2c_project.ino
+++ arduino-i2c-lcd-display/src/lcd_i2c_project.ino
@@ -1,1 +1,20 @@
+#include "lcd_.h"
+#include "delay.h"
+
+void setup() {
+    lcd_init();
+    lcd_print_lines("Ready","to run!");
+    delay_ms(2000);
+}
+
+void loop() {
+    lcd_print_lines("Hello Asmita!", "I2C LCD Ready");
+    delay_ms(2000);
+
+    lcd_print_lines("I2C Address:", "0x27 Working");
+    delay_ms(2000);
+
+    lcd_print_lines("Using Delay", "delay_ms OK");
+    delay_ms(2000);
+}
 
